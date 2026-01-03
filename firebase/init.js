71ed325627ce3fd63917a0ee0e2e7f41f85e87c9
sackri10 <sackri10@gmail.com>
2026-01-04 01:33:06 +0530

    checkin

---
+++ firebase/init.js
@@ -0,0 +1,9 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyCBTYy51JKnEuiJ6olpVGtTmXvbhRf-Cyk",
+  "authDomain": "imdsk28-181f7.firebaseapp.com",
+  "databaseURL": "https://imdsk28-181f7.firebaseio.com",
+  "messagingSenderId": "770846341538",
+  "projectId": "imdsk28-181f7",
+  "storageBucket": "imdsk28-181f7.appspot.com"
+});
